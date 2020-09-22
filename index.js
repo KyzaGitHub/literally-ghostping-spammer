7f99697e21e7a66e6f7636218a72619a550de37e
Kyza <40003440+KyzaGitHub@users.noreply.github.com>
2020-09-22 16:02:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-jk lol
+const { Plugin } = require("powercord/entities");
+
+module.exports = class LiterallyGhostpingSpammer extends Plugin {
+	async startPlugin() {
+    console.log("jk lol");
+	}
+
+	pluginWillUnload() {
+	}
+};
